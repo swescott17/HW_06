d940f2e3fb8bfdf18f1013e16324c6c59756ff2e
swescott17 <swescott17@my.whitworth.edu>
2016-04-19 14:04:00 -0700

    Added Comments to HW06_2.h.

--- HW06_2.h
+++ HW06_2.h
@@ -1,35 +1,35 @@
 //EX05_02
 
 template<typename T>
-bool isSorted(const T list[], int size)
+bool isSorted(const T list[], int size) //returns true is array goes from largest to smallest
 {
-	int p = 0;
+	int p = 0; //counter for the amount of times the array is not sorted
 	for (int i = 0; i < size; i++)
 	{
 		
 		for (int f = 0; f < size; f++)
 		{
-			if (i > f)
+			if (i > f) //is the first number checked larger or smaller than the second number?
 			{
-				if (list[i] < list[f])
+				if (list[i] < list[f]) //the second part (f) in the array comes before the first (i)
 				{
-					p++;
+					p++; //if the second number is larger, then it adds to the counter
 				}
 			}
 			if (i < f)
 			{
-				if (list[i] > list[f])
+				if (list[i] > list[f]) //opposite of above
 				{
-					p++;
+					p++; //if the second number is smaller, then it a
 				}
 			}
 		}
 	}
-	if (p > 1)
+	if (p > 1) //if the counter counted any unsorted numbers it will return false
 	{
 		return false;
 	}
-	else
+	else //there are no unsorted numbers! it's sorted
 	{
 		return true;
 	}
